7148cf2187b032721def3adb2b5ac2991c3e7cc7
Eben chen <ebenchen@ebendemacbook-pro.local>
2018-06-20 15:14:23 +0800

    Initial Commit

---
+++ MINISOApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MINISOApp
+//
+//  Created by Eben chen on 2018/6/20.
+//  Copyright © 2018年 Ebenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MINISOApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MINISOApp
+//
+//  Created by Eben chen on 2018/6/20.
+//  Copyright © 2018年 Ebenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
